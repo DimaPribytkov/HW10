21b765fa87e328a8fc2a59149aef9a856040e25b
ДимаПрибытков <d.pribytkov93@mail.ru>
2024-01-14 21:49:03 +0300

    Задание выполнено!

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -3,23 +3,25 @@ import java.util.Scanner;
 public class Main {
     static String booksInfo = "";
 
-
     public static void main(String[] args) {
         while (true) {
             String name = inputBookName();
             int pageCount = inputPageCount();
-            addBook(name);
-            addBook(name, pageCount);
-            //todo использовать новый метод тут
+            double priceBook = priceBook();
+           addBook(name);
+           addBook(name, pageCount);
+            addBook(name, pageCount, priceBook);
             printInfo();
         }
     }
-
+    private static double priceBook() {
+        System.out.println("Введите цену книги: ");
+        return new Scanner(System.in).nextDouble();
+    }
     public static String inputBookName() {
         System.out.println("Введите название книги:");
         return new Scanner(System.in).nextLine();
     }
-
     public static int inputPageCount() {
         System.out.println("Введите кол-во страниц:");
         return new Scanner(System.in).nextInt();
@@ -29,11 +31,16 @@ public class Main {
         addBook(bookName, 0);
     }
 
-    public static void addBook(String bookName, int pageCount) {
-        booksInfo = booksInfo + bookName + " - " + (pageCount > 0 ? pageCount : "N/A") + " стр.\n";
+    public static void addBook (String bookName, int pageCount) {
+        addBook(bookName, pageCount, 0);
+    }
+    public static void addBook(String bookName, int pageCount, double priceBook) {
+        booksInfo = booksInfo + bookName + " - " + (pageCount > 0 ? pageCount : "N/A") + " стр, " + "Цена - " +
+                (priceBook > 0 ? priceBook + " руб." : "не определено") + "\n";
     }
 
     public static void printInfo() {
+
         System.out.println(booksInfo);
     }
 
